cda9d98591172d57fa29cb1974d79d3230566a3e
Patricia <patricia.valdebenito@usach.cl>
2019-06-21 19:25:18 -0400

    uso new Promise  + readFile + process.argv + .then()

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-// const path = require ('path');
+const path = require('path');
 
 /* ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   */
 
@@ -21,8 +21,21 @@ const fs = require('fs');
                           });
 
 
+/*               INSTANCIA     ----    1  +  2     --- 
+*/
+/*            **                      asincronia                      **          */
+// const fs = require('fs');
+
+new Promise ((resolve,reject) => 
+fs.readFile(process.argv[2],'utf-8',
+     (err,data) => !err ?   resolve(data) : reject(err) )
+           )
+           .then(data => console.log('FILE:\n' + data),
+                 err => console.log('ERROR\n'+ err))
+
 
 /*                INSTANCIA 3 --- COMPROBAR  EXISTENCIA DE ARCHIVO 
+
     Se usa   **    fs.access()     **     para verificar la accesibilidad 
     de un archivo antes de llamar fs.open(), fs.readFile()o fs.writeFile()
 */
@@ -36,9 +49,39 @@ fs.access(file, fs.constants.F_OK, (err) => {
   console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
 });
 
+/*                INSTANCIA 4 --- PATH
 
+   El path.extname()método 
+   devuelve la extensión del path, 
+   desde la última aparición del carácter .(punto)
+   hasta el final de la cadena
+   en la última parte del archivo path. 
+   Si no hay ninguna .en la última parte de la path,
+   o si no hay .otros caracteres que no sean 
+   el primer carácter del nombre base de path
+  (ver path.basename()), se devuelve una cadena vacía.
+*/
 
+//  path.basename (ruta [, ext]) 
+path.basename('/foo/bar/baz/asdf/quux.html');
+// Returns: 'quux.html'
 
+path.basename('/foo/bar/baz/asdf/quux.html', '.html');
+// Returns: 'quux'
+
+path.extname('index.html');
+// Returns: '.html'
+
+path.extname('index.coffee.md');
+// Returns: '.md'
+
+path.extname('index.');
+// Returns: '.'
+
+/* example Ruta Absoluta ----  path.isAbsolute (ruta)
+                            El path.isAbsolute()
+                            método determina si path
+                            es una ruta absoluta. */
 
 // module.exports = () => {
 //   // ...

940854b43968fb44fd2ed59bd0703c8b28a1f84a
Patricia <patricia.valdebenito@usach.cl>
2019-06-21 01:42:19 -0400

    Instancia 3 -  comprobar existencia de archivo fs.acess()

--- app.js
+++ app.js
@@ -6,24 +6,39 @@ const fs = require('fs');
 /*               INSTANCIA 1 --- LECTURA DE PATH -FILE 
 */
 
-
 // usando arrow function
-const pathEnteredUser = () => filePath = process.argv[2];
-const pathEnteredUserFunction = pathEnteredUser();
-   /*    console.log(pathEnteredUser());     */
+ const pathEnteredUser = () => filePath = process.argv[2];
+ console.log(pathEnteredUser());    
 // es valor es tomado para ser leido posteriormente
 
 
 /*               INSTANCIA 2 --- LECTURA DE FILE-SYSTEM
 */
+ readFile = (pathFile) => readFileWithPath =
+                          fs.readFile(pathFile,'utf-8',(error,data) => { 
+                                if(error){ console.log(`Error ${error}`);} 
+                                else { return data; }
+                          });
 
-fs.readFile(pathEnteredUserFunction,'utf-8' ,(error, data) => {
-   if(error){
-       console.log(`Error ${error}`);
-   }else{
-       console.log(data);
-   }
+
+
+/*                INSTANCIA 3 --- COMPROBAR  EXISTENCIA DE ARCHIVO 
+    Se usa   **    fs.access()     **     para verificar la accesibilidad 
+    de un archivo antes de llamar fs.open(), fs.readFile()o fs.writeFile()
+*/
+
+
+//const file example
+const file = 'package.json';
+
+// Check if the file exists in the current directory.
+fs.access(file, fs.constants.F_OK, (err) => {
+  console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
 });
+
+
+
+
 
 // module.exports = () => {
 //   // ...

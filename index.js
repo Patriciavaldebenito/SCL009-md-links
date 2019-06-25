3d9981526d6cf0432093e5e114eff5d61edaecbe
Patricia <patricia.valdebenito@usach.cl>
2019-06-25 16:48:39 -0400

    Se agrega contenido en archivo index.js agregando etiqueta lectura archivo para node usr/bin/ env node, require('marked'), creacion modulo objeto exports mdLinks, copia funcion markdownLinkExtractor

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+/* REFERENCIAS*/
+/* #!/usr/bin/env node --> referencia  https://stackoverflow.com/questions/33509816/what-exactly-does-usr-bin-env-node-do-at-the-beginning-of-node-files */
+
+/* CONSTANTES*/
+const Marked = require('marked');
+
+/* CONSOLE --> console.log('')*/
+/* console.log(process.argv[2]); */
+
+// creacion de modulo
+module.exports = mdLinks;
+
+
+// extraer los links - object de mdLinks
+mdLinks.markdownLinkExtractor = (markdown) => {
+  const links = [];
+
+  const renderer = new Marked.Renderer();
+
+  // Taken from https://github.com/markedjs/marked/issues/1279
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+
+  renderer.link = function(href, title, text) {
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  renderer.image = function(href, title, text) {
+      // Remove image size at the end, e.g. ' =20%x50'
+      href = href.replace(/ =\d*%?x\d*%?$/, '');
+      links.push({
+        href: href,
+        text: text,
+        title: title,
+      });
+  };
+  Marked(markdown, {renderer: renderer});
+
+  return links;
+};
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+
+

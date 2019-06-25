c82e8a999acbd8898f1c7791439c1fb28bdec28a
Patricia <patricia.valdebenito@usach.cl>
2019-06-25 18:39:03 -0400

    Se agrega options(argv) con opcion --validate y --stats

--- app.js
+++ app.js
@@ -1,95 +1,13 @@
-// const fs = require('fs');
-// const path = require('path');
+ const processArgv = process.argv; 
 
-/* ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   ***   */
+//  console.log(processArgv);
+     /* [ '/usr/bin/node',
+          '/home/laboratoriad318/Escritorio/SCL009-md-links/app.js' ] */
 
-/*               INSTANCIA 1 --- LECTURA DE PATH -FILE 
-*/
+//  console.log(processArgv[2]);
+     /* ingreso usuario en consola  */
 
-// usando arrow function
-//  const pathEnteredUser = () => filePath = process.argv[2];
-//  console.log(pathEnteredUser());    
-// es valor es tomado para ser leido posteriormente
+//console.log(require); 
+    /*  objeto .main .Module .exports */
 
-
-/*               INSTANCIA 2 --- LECTURA DE FILE-SYSTEM
-*/
-//  readFile = (pathFile) => readFileWithPath =
-//                           fs.readFile(pathFile,'utf-8',(error,data) => { 
-//                                 if(error){ console.log(`Error ${error}`);} 
-//                                 else { return data; }
-//                           });
-
-
-/*               INSTANCIA     ----    1  +  2     --- 
-*/
-/*            **                      asincronia                      **          */
-// const fs = require('fs');
-
-// new Promise ((resolve,reject) => 
-// fs.readFile(process.argv[2],'utf-8',
-//      (err,data) => !err ?   resolve(data) : reject(err) )
-//            )
-//            .then(data => console.log('FILE:\n' + data),
-//                  err => console.log('ERROR\n'+ err))
-
-
-
-
-/*                INSTANCIA 3 --- COMPROBAR  EXISTENCIA DE ARCHIVO 
-
-    Se usa   **    fs.access()     **     para verificar la accesibilidad 
-    de un archivo antes de llamar fs.open(), fs.readFile()o fs.writeFile()
-*/
-
-
-//const file example
-// const file = 'package.json';
-
-// Check if the file exists in the current directory.
-// fs.access(file, fs.constants.F_OK, (err) => {
-//   console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
-// });
-
-/*                INSTANCIA 4 --- PATH
-
-   El path.extname()método 
-   devuelve la extensión del path, 
-   desde la última aparición del carácter .(punto)
-   hasta el final de la cadena
-   en la última parte del archivo path. 
-   Si no hay ninguna .en la última parte de la path,
-   o si no hay .otros caracteres que no sean 
-   el primer carácter del nombre base de path
-  (ver path.basename()), se devuelve una cadena vacía.
-*/
-
-//  path.basename (ruta [, ext]) 
-// path.basename('/foo/bar/baz/asdf/quux.html');
-// Returns: 'quux.html'
-
-// path.basename('/foo/bar/baz/asdf/quux.html', '.html');
-// Returns: 'quux'
-
-// path.extname('index.html');
-// Returns: '.html'
-
-// path.extname('index.coffee.md');
-// Returns: '.md'
-
-// path.extname('index.');
-// Returns: '.'
-
-/* example Ruta Absoluta ----  path.isAbsolute (ruta)
-                            El path.isAbsolute()
-                            método determina si path
-                            es una ruta absoluta. */
-
-
-
-
-
-
-// module.exports = () => {
-//   // ...
-// };
+console.log(require.main); // node_modules
--- index.js
+++ index.js
@@ -4,48 +4,81 @@
 /* #!/usr/bin/env node --> referencia  https://stackoverflow.com/questions/33509816/what-exactly-does-usr-bin-env-node-do-at-the-beginning-of-node-files */
 
 /* CONSTANTES*/
-const Marked = require('marked');
+//const Marked = require('marked');
+let  positionOption = process.argv[2];
+//console.log(positionOption);
 
 /* CONSOLE --> console.log('')*/
 /* console.log(process.argv[2]); */
 
-// creacion de modulo
-module.exports = mdLinks;
+//   options: Un objeto con las siguientes propiedades:   - validate: Booleano que determina si se desea validar los links encontrados.
+//            --validate y --stats
+
+const  options = (argv) => { 
+  
+   if(argv === '--stats' ){
+    console.log('la posicion es --stats');
+    let stats = true;
+    return stats;
+   } 
+
+   if(argv === '--validate' ){
+    console.log('la posicion es --validate');
+    let validate = true; 
+    return validate;
+  
+  } 
+ else{
+   console.log("el ingreso es diferente ");
+ }
+}
+
+options(positionOption);
 
 
+
+//console.log(require.main);
 // extraer los links - object de mdLinks
-mdLinks.markdownLinkExtractor = (markdown) => {
-  const links = [];
+// mdLinks.markdownLinkExtractor = (markdown) => {
+//   const links = [];
 
-  const renderer = new Marked.Renderer();
+//   const renderer = new Marked.Renderer();
 
-  // Taken from https://github.com/markedjs/marked/issues/1279
-  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+//   // Taken from https://github.com/markedjs/marked/issues/1279
+//   const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
 
-  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+//   Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+//   Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+//   Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
 
-  renderer.link = function(href, title, text) {
-    links.push({
-      href: href,
-      text: text,
-      title: title,
-    });
-  };
-  renderer.image = function(href, title, text) {
-      // Remove image size at the end, e.g. ' =20%x50'
-      href = href.replace(/ =\d*%?x\d*%?$/, '');
-      links.push({
-        href: href,
-        text: text,
-        title: title,
-      });
-  };
-  Marked(markdown, {renderer: renderer});
+//   renderer.link = function(href, title, text) {
+//     links.push({
+//       href: href,
+//       text: text,
+//       title: title,
+//     });
+//   };
+//   renderer.image = function(href, title, text) {
+//       // Remove image size at the end, e.g. ' =20%x50'
+//       href = href.replace(/ =\d*%?x\d*%?$/, '');
+//       links.push({
+//         href: href,
+//         text: text,
+//         title: title,
+//       });
+//   };
+//   Marked(markdown, {renderer: renderer});
 
-  return links;
-};
+//   return links;
+// };
+
+// creacion de modulo
+// module.exports = () => {
+//   // ...
+// };
+let mdLinks = {a:2};
+module.exports = mdLinks;
+//console.log(mdLinks);
 /* */
 /* */
 /* */

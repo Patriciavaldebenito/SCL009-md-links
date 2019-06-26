3ed3fd12495fe6c63137db19a05b07fb333f7830
Patricia <patricia.valdebenito@usach.cl>
2019-06-26 01:06:49 -0400

    exportacion y uso /prueba con console de mdLink.mdLink

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const mdLink = require('/home/laboratoriad318/Escritorio/SCL009-md-links/mdLink.js').mdLink; 
 
-mdLink(2,3)
+
+const pathRequest  = process.argv[2];
+const options = process.argv[3]; 
+
+function executionModule(){
+
+    mdLink(pathRequest,options);
+}
+
+executionModule();
--- mdLink.js
+++ mdLink.js
@@ -1,9 +1,21 @@
 let mdLink = {};
 
-mdLink.mdLink = (a,b) => {
-    let suma = a+ b;
-    console.log(suma);
-    
+mdLink.mdLink = (pathRequested, options) => {
+    // probando con console ingreso 
+    console.log(pathRequested);  console.log(options);
+
+ /*   1. validar opcion  / --validate or -- stats   */
+
+ /*   2. validar pathRequested / ruta     */
+}
+
+
+mdLink.sum = (a,b) => {
+
+   let sum = a + b;
+   return sum;
+
+   
 }
 
 

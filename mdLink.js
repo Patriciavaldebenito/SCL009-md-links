91806a407652b48830a3cd4745dc24321d84779c
Patricia <patricia.valdebenito@usach.cl>
2019-07-01 11:14:23 -0400

    declaration of modules -   declaracion de modulos

--- mdLink.js
+++ mdLink.js
@@ -1,2 +1,9 @@
-// * 0 *  Declaración de variables 
+// * 0 *  Declaration of variables
 let mdLinks = {};
+
+// * 1 * declaration of modules
+const fs = require('fs');
+const path = require('path');
+const fileHound = require('filehound');
+const marked = require('marked');
+const fetch = require('fetch');

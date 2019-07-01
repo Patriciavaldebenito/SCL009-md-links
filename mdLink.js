f6f2051eacb6c779ab45183e05b5184825bdcabc
Patricia <patricia.valdebenito@usach.cl>
2019-07-01 11:15:41 -0400

    // * 2 *   IMPORTED MODULE IN INDEX.JS INDEX.JS

--- mdLink.js
+++ mdLink.js
@@ -7,3 +7,22 @@ const path = require('path');
 const fileHound = require('filehound');
 const marked = require('marked');
 const fetch = require('fetch');
+
+// * 2 *   IMPORTED MODULE IN INDEX.JS INDEX.JS 
+mdLinks.mdLinks = (pathInConsole, options) => {
+  
+    if (options === '--validate') {
+        let options = {};
+        console.log(" options.validate" + options.validate);
+        return options.validate = true; 
+    }
+    // secuencia 
+    // * 3 *  obteniendo ruta absoluta 
+    let pathExecute = mdLinks.pathConvertAbsolute(pathInConsole);
+    
+    // * 4 *  saber- archivo o directorio - ejecutar function correspondiente.
+  
+    mdLinks.callFileOrDirectory(pathExecute); // * clasificando is File Or Directory *
+    
+   
+}
